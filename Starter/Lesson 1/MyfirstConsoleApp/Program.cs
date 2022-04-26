4ca049d9b8f20402ed918b76b69cf5cce0b42ace
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-04-26 15:16:50 +0300

    Created MyfirstConsoleApp

---
+++ Starter/Lesson 1/MyfirstConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyfirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(2+2);
+            Console.ReadKey();
+        }
+    }
+}
