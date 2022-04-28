463be6c0d81e7bc1a3a8fc9f6a828b29349aeaf0
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-04-28 12:32:01 +0300

    Clear code, and make change's to MessageBoxes

--- Starter/Lesson 2/WindowsFormsAppHomeWork2/Form1.cs
+++ Starter/Lesson 2/WindowsFormsAppHomeWork2/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsAppHomeWork2
@@ -19,42 +12,42 @@ namespace WindowsFormsAppHomeWork2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, i'm Byte");
+            MessageBox.Show("from 0 to 255");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, i'm sByte");
+            MessageBox.Show("from -128 to 127");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, i'm short");
+            MessageBox.Show("from -32768 to 32767");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, i'm uShort");
+            MessageBox.Show("from 0 to 65535");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, i'm int");
+            MessageBox.Show("from -2147483648 to 2147483647");
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, i'm uInt");
+            MessageBox.Show("from 0 to 4294967295");
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, i'm long");
+            MessageBox.Show("from –9 223 372 036 854 775 808 to 9 223 372 036 854 775 807");
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, i'm uLong");
+            MessageBox.Show("from 0 to 18 446 744 073 709 551 615");
         }
     }
 }
