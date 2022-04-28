3712b9f8ab4f8fd228331e1e318a5b71d1311a81
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-04-28 14:15:17 +0300

    Make HomeWork2 , "N" add Line skip, "T" add Start from the middle of the line, "A" make sound

---
+++ Starter/Lesson 2/ConsoleAppHomeWork2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHomeWork2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c;
+            a = "\nMyString1";
+            b = "\tMyString2";
+            c = "\aMyString3";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
