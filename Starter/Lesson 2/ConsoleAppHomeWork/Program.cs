95928eb33df430fa3fc0348297201f3631289ed4
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-04-28 13:16:16 +0300

    Make console app. For pi use Double, for E use Decimal

---
+++ Starter/Lesson 2/ConsoleAppHomeWork/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAppHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592653;
+            decimal e = 2.71828182845904523536M;
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+            Console.ReadLine();
+            
+        }
+    }
+}
