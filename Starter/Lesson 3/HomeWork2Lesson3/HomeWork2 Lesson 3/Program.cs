96542e035f01f7fb335c7ea02b632eeffb2132cb
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-04-29 16:06:57 +0300

    Calculate the Average on c#

---
+++ Starter/Lesson 3/HomeWork2Lesson3/HomeWork2 Lesson 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork2_Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 11, z = 3;
+            float av;
+            av = (x + y + z) / 3;
+            Console.WriteLine("Среднее арифметичесское чисел {0}, {1}, {2} = {3}", x, y, z, av);
+            Console.ReadKey();
+        }
+    }
+}
