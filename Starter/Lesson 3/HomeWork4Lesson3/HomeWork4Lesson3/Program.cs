bed9ebacc030a43b2a9dc4d247e7c29d23da11f4
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-04-30 12:28:02 +0300

    HomeWork4Lesson3

---
+++ Starter/Lesson 3/HomeWork4Lesson3/HomeWork4Lesson3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork4Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            double R;
+            R = 5;
+            double h = 3;
+            double V;
+            V = PI * (R * R) * h;
+            Console.WriteLine(V);
+            Console.ReadKey();
+
+        }
+    }
+}
