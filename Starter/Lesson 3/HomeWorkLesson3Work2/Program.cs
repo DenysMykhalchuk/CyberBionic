01444542a6ae171240009058e492bf976ad194bf
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-04-30 11:20:25 +0300

    HomeWork3 Lesson 3

---
+++ Starter/Lesson 3/HomeWorkLesson3Work2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkLesson3Work2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 12;
+            int z = 3;
+            x += y - x++ * z;
+            z = --x - y * 5;
+            Console.WriteLine($"x = {x}\ny = {y}\nz = {z}");
+            y /= x + 5 % z;
+            z = x++ + y * 5;
+            x = y - x++ * z;
+            Console.WriteLine($"x = {x}\n y = {y}\nz = {z}");
+            Console.Read();
+        }
+    }
+}
