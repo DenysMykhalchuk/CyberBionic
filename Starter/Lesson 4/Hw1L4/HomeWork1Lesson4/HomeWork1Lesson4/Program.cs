c6f91c7705d3b39f6f9dfb2aa210fb7eee854000
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-05-07 12:23:49 +0300

    Hw1L4

---
+++ Starter/Lesson 4/Hw1L4/HomeWork1Lesson4/HomeWork1Lesson4/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace HomeWork1Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int operand1 = 9, operand2 = 3;
+            Console.Write("Введите operand 1\n");
+            string operands = Console.ReadLine();
+            Console.Write("Введите operand 2\n");
+            operands = Console.ReadLine();
+            operand1 = Convert.ToInt32(operands);
+            operand2 = Convert.ToInt32(operands);
+            Console.Write("Введите знак арифметичесского действия -");
+            string arf = Console.ReadLine();
+            switch (arf)
+            {
+                case "+":
+                {
+                    int rez = operand1 + operand2;
+                    Console.WriteLine(rez);
+                    break;
+                }
+                case "-":
+                {
+                    int rez = operand1 - operand2;
+                    Console.WriteLine(rez);
+                    break;
+                }
+                case "*":
+                {
+                    int rez = operand1 * operand2;
+                    Console.WriteLine(rez);
+                    break;
+                }
+                case "/":
+                {
+                    if (operand2 != 0)
+
+                    {
+                        int rez = operand1 / operand2;
+                        Console.WriteLine(rez);
+                        break;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("На 0 делить запрещено!\n");
+                    }
+
+                    break;
+
+
+                }
+                case "%":
+                {
+                    if (operand2 != 0)
+                    {
+                        int rez = operand1 % operand2;
+                        Console.WriteLine(rez);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("На 0 делить запрещено!\n");
+                    }
+
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Ошибка ввода, выберите другой арифметичесский знак/n");
+                    break;
+                }
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Starter/Lesson 4/Hw1L4/HomeWork1Lesson4/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace L4HWT2_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int operand1 = 0, operand2 = 0;
+                Console.Write("Введите операнд 1 - ");
+                string operands = Console.ReadLine();
+                operand1 = Convert.ToInt32(operands);
+                Console.Write("Введите операнд 2 - ");
+                operands = Console.ReadLine();
+                operand2 = Convert.ToInt32(operands);
+                Console.Write("Введите знак арифметического действия - ");
+                string arf = Console.ReadLine();
+                switch (arf)
+                {
+                    case "+":
+                        {
+                            int rez = operand1 + operand2;
+                            Console.WriteLine(rez);
+                            break;
+                        }
+                    case "-":
+                        {
+                            int rez = operand1 - operand2;
+                            Console.WriteLine(rez);
+                            break;
+                        }
+                    case "*":
+                        {
+                            int rez = operand1 * operand2;
+                            Console.WriteLine(rez);
+                            break;
+                        }
+                    case "/":
+                        {
+                            if (operand2 != 0)
+                            {
+                                int rez = operand1 / operand2;
+                                Console.WriteLine(rez);
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("На 0 делить запрещено!\n");
+                            }
+                            break;
+                        }
+                    case "%":
+                        {
+                            if (operand2 != 0)
+                            {
+                                int rez = operand1 % operand2;
+                                Console.WriteLine(rez);
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("На 0 делить запрещено!\n");
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Ошибка ввода, выберите другой арифметический знак\n");
+                            break;
+                        }
+                }
+            }
+            while (Ques());
+            bool Ques()
+            {
+                Console.Write("Желаете продолжить?(Y/N) ");
+                string choise = Console.ReadLine();
+                if (choise == "N") return false;
+                else return true;
+            }
+            void Output(int operand1, int operand2, string arifmat, int rez)
+            {
+                Console.WriteLine("{0} {3} {1} = {2}", operand1, operand2, rez, arifmat);
+            }
+        }
+    }
+}
