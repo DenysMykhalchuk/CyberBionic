10da4bbea0fabfadd09dd37cd72ee6e7b43c79f1
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2022-05-07 14:29:04 +0300

    Clear Code, Make homework2 , console app with Interval , use "else if"

--- Starter/Lesson 4/Hw2L4/HW2Lesson4/Program.cs
+++ Starter/Lesson 4/Hw2L4/HW2Lesson4/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HW2Lesson4
 {
@@ -10,6 +6,14 @@ namespace HW2Lesson4
     {
         static void Main(string[] args)
         {
+            Console.Write("Input Number - ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (b < 15) Console.WriteLine("[0-14]");
+            else if (b < 36) Console.WriteLine("[15-35]");
+            else if (b < 51) Console.WriteLine("[36-50]");
+            else if (b < 101) Console.WriteLine("[50-100]");
+            else if (b != 0 - 100) Console.WriteLine("Error Input Number");
+            Console.ReadKey();
         }
     }
 }
